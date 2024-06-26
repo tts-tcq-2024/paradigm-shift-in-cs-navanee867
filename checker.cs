4f407a7a97f844887f8bb5d5c83f7993b68c91b2
navanee867 <navanee777@gmail.com>
2024-06-26 23:03:50 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -13,12 +13,12 @@ class Checker
         {
             if (temperature < 0)
             {
-                Console.WriteLine("Temperature is too low!");
+                PrintMessage("Temperature is too low!");
                 return false;
             }
             else if(temperature > 45)
             {
-                Console.WriteLine("Temperature is too high!");
+                PrintMessage("Temperature is too high!");
                 return false;
             }
 
@@ -29,12 +29,12 @@ class Checker
         {
             if (soc < 20)
             {
-                Console.WriteLine("State of Charge is too low!");
+                PrintMessage("State of Charge is too low!");
                 return false;
             }
             else if (soc > 80)
             {
-                Console.WriteLine("State of Charge is too high!");
+                PrintMessage("State of Charge is too high!");
                 return false;
             }
 
@@ -45,7 +45,7 @@ class Checker
         {
             if (chargeRate > 0.8)
             {
-                Console.WriteLine("Charge Rate is out of range!");
+                PrintMessage("Charge Rate is out of range!");
                 return false;
             }
 
@@ -56,7 +56,7 @@ class Checker
         {
             if (!expression)
             {
-                Console.WriteLine("Expected true, but got false");
+                PrintMessage("Expected true, but got false");
                 Environment.Exit(1);
             }
         }
@@ -64,10 +64,14 @@ class Checker
         {
             if (expression)
             {
-                Console.WriteLine("Expected false, but got true");
+                PrintMessage("Expected false, but got true");
                 Environment.Exit(1);
             }
         }
+       static void PrintMessage(string message)
+       {
+            Console.WriteLine(message);
+       }
         static int Main()
         {
             ExpectTrue(BatteryIsOk(25, 70, 0.7f));
