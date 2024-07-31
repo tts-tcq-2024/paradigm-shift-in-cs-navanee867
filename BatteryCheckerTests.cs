3cba38f1d5e7d91fc486c26eaa7750581462d6f0
navanee867 <navanee777@gmail.com>
2024-07-31 18:09:07 +0530

    Create BatteryCheckerTests.cs

---
+++ BatteryCheckerTests.cs
@@ -0,0 +1,20 @@
+using Xunit;
+using static paradigm_shift_csharp.BatteryCheckerHelper;
+
+namespace paradigm_shift_csharp
+{
+    internal class BatteryCheckerTests
+    {
+        public static void BatteryCheckTests()
+        {
+            Assert.True(BatteryIsOk(25, 70, 0.7f));
+            Assert.True(BatteryIsOk(0, 80, 0.7f));
+            Assert.True(BatteryIsOk(45, 20, 0.7f));
+            Assert.False(BatteryIsOk(-1, 80, 0.7f));
+            Assert.False(BatteryIsOk(10, 85, 0.7f));
+            Assert.False(BatteryIsOk(50, 70, 0.0f));
+            Assert.False(BatteryIsOk(30, 70, 0.9f));
+            Console.WriteLine("All ok");
+        }
+    }
+}
