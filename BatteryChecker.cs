8489e61d001562d9c384bedc39ed9689e12566fa
navanee867 <navanee777@gmail.com>
2024-07-31 18:04:11 +0530

    Update and rename checker.cs to BatteryChecker.cs

---
+++ BatteryChecker.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Diagnostics;
+namespace paradigm_shift_csharp
+{
+    internal class BatteryChecker
+    {
+        static int Main()
+        {
+            BatteryCheckerTests.BatteryCheckerTests();
+            return 0;
+        }
+    }
+}
