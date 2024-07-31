01d7ceebbffdd15ca395def4c004cbafcb26782d
navanee867 <navanee777@gmail.com>
2024-07-31 18:16:08 +0530

    Update BatteryChecker.cs

--- BatteryChecker.cs
+++ BatteryChecker.cs
@@ -1,12 +1,22 @@
 using System;
 using System.Diagnostics;
+using Xunit;
+using static paradigm_shift_csharp.BatteryCheckerHelper;
+
 namespace paradigm_shift_csharp
 {
     internal class BatteryChecker
     {
         static int Main()
         {
-            BatteryCheckerTests.BatteryCheckTests();
+            Assert.True(BatteryIsOk(25, 70, 0.7f));
+            Assert.True(BatteryIsOk(0, 80, 0.7f));
+            Assert.True(BatteryIsOk(45, 20, 0.7f));
+            Assert.False(BatteryIsOk(-1, 80, 0.7f));
+            Assert.False(BatteryIsOk(10, 85, 0.7f));
+            Assert.False(BatteryIsOk(50, 70, 0.0f));
+            Assert.False(BatteryIsOk(30, 70, 0.9f));
+            Console.WriteLine("All ok");
             return 0;
         }
     }
